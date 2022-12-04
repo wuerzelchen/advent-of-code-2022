b3e73111f5e52ff1f19ad0d848fcc2c842a87b61
Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>
2022-12-04 18:24:41 +0100

    feat(day4.go): part two
    Signed-off-by: Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>

--- day4.go
+++ day4.go
@@ -19,8 +19,25 @@ func isContained(a1 int, a2 int, b1 int, b2 int) bool {
 	}
 }
 
+// 1-5,3-7 = overlapping
+// 1-3,4-6 = not overlapping
+// 1-3,3-8 = overlapping
+// 8-9,2-9 = overlapping
+// 5-9,6-8 = overlapping
+// 1-6,2-4 = overlapping
+// 7-8,1-3 = not overlapping
+// 1-7,4-9 = overlapping
+func isOverlapping(a1 int, a2 int, b1 int, b2 int) bool {
+	if a1 > b2 || a2 < b1 {
+		return false
+	} else {
+		return true
+	}
+}
+
 func day4() {
 	sum := 0
+	overlappingSum := 0
 	//read contents of file from disk
 	data, err := ioutil.ReadFile("day4input.txt")
 	if err != nil {
@@ -56,7 +73,11 @@ func day4() {
 		if isContained(a1, a2, b1, b2) {
 			sum++
 		}
+		if isOverlapping(a1, a2, b1, b2) {
+			overlappingSum++
+		}
 
 	}
-	fmt.Println(sum)
+	fmt.Println("containing sum: ", sum)
+	fmt.Println("overlapping sum: ", overlappingSum)
 }
